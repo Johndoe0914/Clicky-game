7c96a2aa358bdf5950528ce969dffd12523df98b
Jonathan Diaz <jonathandiaz0914@gmail.com>
2019-04-10 00:33:30 -0400

    Updates

---
+++ precache-manifest.6325c0416be6e95daa016a6c73bf26ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3bc0314d2fe2b101312e",
+    "url": "/static/js/main.e3ea5290.chunk.js"
+  },
+  {
+    "revision": "c4cc6779ce149918c99f",
+    "url": "/static/js/2.201173ab.chunk.js"
+  },
+  {
+    "revision": "3bc0314d2fe2b101312e",
+    "url": "/static/css/main.290b6090.chunk.css"
+  },
+  {
+    "revision": "477be9a8a51eadcb88f14757a74f725e",
+    "url": "/index.html"
+  }
+];
